b38b7f4f51eb33e9344d40bcd077c8ba2b78a4c4
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-12 11:52:39 +0300

    update script

---
+++ tictactoe/script.js
@@ -0,0 +1,15 @@
+var item1 = document.getElementByID("item1");
+var item2 = document.getElementByID("item2");
+var item3 = document.getElementByID("item3");
+var item4 = document.getElementByID("item4");
+var item5 = document.getElementByID("item5");
+var item6 = document.getElementByID("item6");
+var item7 = document.getElementByID("item7");
+var item8 = document.getElementByID("item8");
+var item9 = document.getElementByID("item9");
+
+document.getElementById("addp").innerHTML = "<p>Hello</p>";
+
+function changeSymbol(){
+
+}
