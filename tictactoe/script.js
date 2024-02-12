ebea1d7e3735cb04f85c07244873bbd90c8eaa57
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-12 12:20:15 +0300

    Update script.js

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,5 +1,5 @@
 function setSymbol(){
-    var cell = document.getElementById("field");
+    var cell = document.getElementById("field").value;
     document.getElementById("item"+cell).style.backgroundImage = "cross.jpg";
     window.alert(cell);
 
